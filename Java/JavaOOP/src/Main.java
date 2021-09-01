e1af1ca967ff442bd9d62c75c98de2831ee7e039
mateooviedo <mateooviedotorres@gmail.com>
2021-08-31 23:05:19 -0500

    Metodo constructor

--- Java/JavaOOP/src/Main.java
+++ Java/JavaOOP/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro";
+        Doctor myDoctor = new Doctor("Anahi Salgado");
+        myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
     }
 }
