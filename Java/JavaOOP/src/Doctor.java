838eea27317a2184febc838afe0c28ae2c375b5e
mateooviedo <mateooviedotorres@gmail.com>
2021-08-31 23:33:20 -0500

    Variables and Methods Statics

--- Java/JavaOOP/src/Doctor.java
+++ Java/JavaOOP/src/Doctor.java
@@ -1,10 +1,11 @@
 public class Doctor {
-    int id;
+    int id; // Autoincrement
     String name;
     String speciality;
 
     Doctor() {
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
 
     Doctor(String name) {
@@ -14,5 +15,9 @@ public class Doctor {
     // Behaviours
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 }
--- Java/JavaOOP/src/Main.java
+++ Java/JavaOOP/src/Main.java
@@ -1,7 +1,15 @@
+import static ui.UiMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Anahi Salgado");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
     }
 }
