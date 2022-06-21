cd24c4ac1f360a2fae9b7ececc942e40d7606442
mateooviedo <mateooviedotorres@gmail.com>
2022-06-20 23:22:47 -0500

    test

--- Java/JavaOOP/src/Patient.java
+++ Java/JavaOOP/src/Patient.java
@@ -10,7 +10,71 @@ public class Patient {
   private String blood;
 
   Patient(String name, String email) {
+    this.setName(name);
+    this.setEmail(email);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
     this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
     this.email = email;
+  }
+
+  public String getAddres() {
+    return addres;
+  }
+
+  public void setAddres(String addres) {
+    this.addres = addres;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(String birthday) {
+    this.birthday = birthday;
+  }
+
+  public double getWeight() {
+    return weight;
+  }
+
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+  public String getBlood() {
+    return blood;
+  }
+
+  public void setBlood(String blood) {
+    this.blood = blood;
   }
 }
