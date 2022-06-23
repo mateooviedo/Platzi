00af917cfc4a018696c05683cddfbf01c300f1c1
mateooviedo <mateooviedotorres@gmail.com>
2022-06-22 22:21:01 -0500

    fmt package

--- GO/basic-go/src/main.go
+++ GO/basic-go/src/main.go
@@ -3,30 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// Constat declaration
-	const pi float64 = 3.14
-	const pi2 = 3.1415
+	// fmt package
+	nombre := "Platzi"
+	cursos := 500
 
-	fmt.Println("pi", pi)
-	fmt.Println("pi2", pi2)
+	// Println
+	fmt.Println(nombre)
 
-	// Variables declaration
-	base := 12
-	var altura int = 14
-	var area int
+	// Printf
+	fmt.Printf("%s tiene mas de %d cursos\n", nombre, cursos)
 
-	fmt.Println(base, altura, area)
-
-	// Zero values (values by default in go)
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a, b, c, d)
-
-	// Area de cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println("el area del cuadrado es", areaCuadrado)
+	// Sprintf
+	message := fmt.Sprintf("%s tiene mas de %d cursos", nombre, cursos)
+	fmt.Println(message)
 }
